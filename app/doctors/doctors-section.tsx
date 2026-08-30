217ba3abdc3f7c71a1d3396cc22ffa5b076b967b
MR-Amr-Emam <amradar757@gmail.com>
2026-08-30 17:52:19 +0300

    trying to merge

--- app/doctors/doctors-section.tsx
+++ app/doctors/doctors-section.tsx
@@ -56,14 +56,4 @@ function DoctorCard(){
 }
 
 
-function TestComp({children}:{children:ReactNode}){
-    const [state, setState] = useState(0);
-    return(
-        <div>
-            <div className="my-2"><button className="btn btn-danger" onClick={()=>{setState(state+1)}}>add</button>{state}</div>
-            <div className="my-2">
-                {children}
-            </div>
-        </div>
-    )
-}
+
